acac2397c9ad048f0f0c93f7423a404c8594b86f
zahid <zahid.galea@wom.cl>
2021-11-24 12:20:00 -0300

    Adds automatic rollback flag

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 
-gcloud deployment-manager deployments create testlink --template=testlink.jinja --properties="zone:'us-east1-b'"
+gcloud deployment-manager deployments create testlink --template=testlink.jinja --properties="zone:'us-east1-b'" --automatic-rollback-on-error
